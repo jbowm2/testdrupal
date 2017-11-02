44af95a301e95188cf0c6b2214246768a66c5ad7
root <root@homestead>
2017-11-02 19:35:06 +0000

    going to staging

--- web/themes/bootsub/js/site.js
+++ web/themes/bootsub/js/site.js
@@ -1,2 +1,60 @@
 $ = jQuery;
 
+function initSliderBackgroundImage() {
+    $('.paragraph--type--hero-slider-section .wrapper').each(function() {
+        var heroImage = $(".field--name-field-slide-image img").attr("src");
+        $(this).css("background-image", "url('" + heroImage + "')");
+    });
+}
+
+function initEventBackgroundImage() {
+    $('.field--name-field-event .event-square').each(function() {
+       var eventImage = $(this).find(".field--name-field-event-image img").attr("src");
+       $(this).css("background-image", "url('" + eventImage + "')");
+    });
+}
+
+function initHideProfileElements() {
+    $('.field--name-dynamic-block-fieldnode-people-profiles .views-row').each(function() {
+        if ($(this).find('.views-field-field-webpage .field-content a').length) {
+            $(this).find('.views-field-field-webpage').show();
+        }
+        else {
+            $(this).find('.views-field-field-webpage').hide();
+        }
+    });
+}
+
+function initBannerImageBackground() {
+    $(".group-header .field--name-field-banner-image").each(function() {
+        var bannerImage = $(".field--name-field-banner-image img").attr("src");
+        $(this).css("background-image", "url('" + bannerImage + "')");
+    });
+}
+
+function initSquares() {
+    resizeSquares();
+}
+
+function resizeSquares() {
+    $('.event-square').each(function() {
+        var imgWidth = $(this).width();
+        $(this).height(imgWidth);
+    })
+}
+
+
+$(document).ready(function() {
+    initSliderBackgroundImage();
+    initHideProfileElements();
+    initBannerImageBackground();
+    initEventBackgroundImage();
+    initSquares();
+});
+
+$(window).resize(function() {
+    resizeSquares();
+});
+
+$( document ).ajaxComplete(function() {
+});
